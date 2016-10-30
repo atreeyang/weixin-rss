430e21da2dcac9e8f47915e642a23578a3357a76
yanchengyang <yanchengyang@souche.com>
2016-10-30 21:50:21 +0800

    redis

--- config/global.js
+++ config/global.js
@@ -6,7 +6,7 @@ module.exports = {
   redis: {
       host: process.env.REDIS_PORT_6379_TCP_ADDR,
       port: process.env.REDIS_PORT_6379_TCP_PORT,
-      autu: process.env.REDIS_PASSWORD,
+      autu: process.env.REDIS_PASSWORD
   },
 
   proxy: 'http://114.36.105.161:8088'
