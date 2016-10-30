ad5fc290396fdc407c4ec4598ddafdec50a59983
yanchengyang <yanchengyang@souche.com>
2016-10-30 21:30:54 +0800

    redis

--- config/global.js
+++ config/global.js
@@ -1,11 +1,12 @@
 module.exports = {
 
-
   proxyEnable: false,
+
 
   redis: {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+      autu: process.env.REDIS_AUTH,
   },
 
   proxy: 'http://114.36.105.161:8088'
